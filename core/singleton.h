76720c6d73b825c4b6e053123ea2923979ad2255
Laszlo Papp <djszapi@archlinux.us>
2011-02-01 00:08:44 +0200

    Initial commit. Basic CMakeLists.txt and core folder.

---
+++ core/singleton.h
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * This file is part of the Gluon Development Platform
+ * Copyright (c) 2010 Laszlo Papp <djszapi@archlinux.us>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef MULA_CORE_SINGLETON
+#define MULA_CORE_SINGLETON
+
+#include <QtCore/QObject>
+#include <QtCore/QtGlobal>
+
+namespace MulaCore
+{
+    template <typename T>
+    class Singleton : public QObject
+    {
+        public:
+            static T* instance()
+            {
+                if( !m_instance )
+                {
+                    m_instance = new T;
+                }
+                return m_instance;
+            }
+
+        protected:
+            static T* m_instance;
+    };
+}
+
+#ifdef Q_OS_WIN32
+#define MULA_DEFINE_SINGLETON(Type) template<> Type* MulaCore::Singleton<Type>::m_instance = 0;
+#else
+#define MULA_DEFINE_SINGLETON(Type) template<> Q_DECL_EXPORT Type* MulaCore::Singleton<Type>::m_instance = 0;
+#endif
+
+#endif //GLUON_CORE_SINGLETON
