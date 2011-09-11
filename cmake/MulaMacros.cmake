b1f37048cdc6c7982b5adae0d00b8d38142c6dbb
Laszlo Papp <ext-laszlo.papp@nokia.com>
2011-09-11 14:30:31 +0300

    Add a helper macro that will simplify the testing build setup for subfolders

---
+++ cmake/MulaMacros.cmake
@@ -0,0 +1,25 @@
+# This file contains macros that are used inside the Mula Project
+
+MACRO(MULA_UNIT_TESTS libraries modulename)
+    FOREACH(testname ${ARGN})
+        qt4_automoc(${testname}.cpp)
+        add_executable("${modulename}-${testname}" ${testname}.cpp)
+        target_link_libraries("${modulename}-${testname}" ${libraries} ${QT_QTTEST_LIBRARY})
+        add_test("${modulename}-${testname}" ${modulename}-${testname})
+        if(WINCE)
+            target_link_libraries("${modulename}-${testname}" ${WCECOMPAT_LIBRARIES})
+        endif(WINCE)
+    ENDFOREACH(testname)
+ENDMACRO(MULA_UNIT_TESTS)
+
+MACRO(MULA_EXECUTABLE_TESTS libraries modulename1)
+    FOREACH(testname ${ARGN})
+        qt4_automoc(${testname}.cpp)
+        add_executable("${modulename}-${testname}" ${testname}.cpp)
+        target_link_libraries("${modulename}-${testname}" ${libraries} ${QT_QTTEST_LIBRARY})
+        if(WINCE)
+            target_link_libraries("${modulename}-${testname}" ${WCECOMPAT_LIBRARIES})
+        endif(WINCE)
+    ENDFOREACH(testname)
+ENDMACRO(MULA_EXECUTABLE_TESTS)
+
