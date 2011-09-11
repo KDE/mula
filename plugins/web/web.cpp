85d760b6c58afabe819d876b912e1ce1c8f741f1
Laszlo Papp <ext-laszlo.papp@nokia.com>
2011-09-11 17:17:50 +0300

    Add the initial version of the web plugin adopted

---
+++ plugins/web/web.cpp
@@ -0,0 +1,135 @@
+/******************************************************************************
+ * This file is part of the MULA project
+ * Copyright (C) 2008 Alexander Rodin
+ * Copyright (c) 2011 Laszlo Papp <lpapp@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "web.h"
+
+#include "settingsdialog.h"
+
+#include <QBuffer>
+#include <QDir>
+#include <QEventLoop>
+#include <QFile>
+#include <QHttp>
+#include <QSettings>
+#include <QUrl>
+#include <QTextCodec>
+
+class Web::Private
+{
+    public:
+        Private()
+        {   
+        }
+
+        ~Private()
+        {
+        }
+
+        struct QueryStruct
+        {   
+            QString query;
+            QByteArray codec;
+        };
+        QHash<QString, QueryStruct> loadedDicts;
+}
+
+Web::Web(QObject *parent)
+    : QObject(parent)
+{
+}
+
+QStringList Web::availableDictionaries() const
+{
+    QStringList result = QDir(workPath()).entryList(QStringList("*.webdict"), QDir::Files, QDir::Name);
+    result.replaceInStrings(".webdict", "");
+    return result;
+}
+
+void Web::setLoadedDictionaries(const QStringList &dictionaries)
+{
+    for (QStringList::const_iterator i = dictionaries.begin(); i != dictionaries.end(); ++i)
+    {
+        QString filename = workPath() + "/" + *i + ".webdict";
+        if (!QFile::exists(filename))
+            continue;
+
+        QSettings dictionary(filename, QSettings::IniFormat);
+        QString query = dict.value("query").toString();
+        if (!query.isEmpty())
+        {
+            d->loadedDictionaries[*i].query = query;
+            d->loadedDictionaries[*i].codec = dictionary.value("charset").toByteArray();
+        }
+    }
+}
+
+Web::DictionaryInfo Web::dictionaryInfo(const QString &dictionary)
+{
+    QString filename = workPath() + "/" + dict + ".webdict";
+    if (!QFile::exists(filename))
+        return DictionaryInfo();
+
+    QSettings dictFile(filename, QSettings::IniFormat);
+    DictionaryInfo info(name(), dictionary, dictionaryFile.value("author").toString(),
+            dictionaryFile.value("description").toString());
+
+    return info;
+}
+
+bool Web::isTranslatable(const QString &dictionary, const QString &word)
+{
+    if (!d->loadedDictionaries.contains(dictionary))
+        return false;
+
+    // TODO: Implement the boolean query
+    Q_UNUSED(word);
+    return true;
+}
+
+Web::Translation Web::translate(const QString &dictionary, const QString &word)
+{
+    if (!d->loadedDictionaries.contains(dictionary))
+        return Translation();
+
+    QUrl url(d->loadedDictionaries[dictionary].query.replace("%s", word));
+    QEventLoop loop;
+    QHttp http(url.host(), url.port(80), &loop);
+    connect(&http, SIGNAL(done(bool)), &loop, SLOT(quit()));
+    http.get(url.path() + "?" + url.encodedQuery());
+    loop.exec();
+
+    QTextCodec *codec = QTextCodec::codecForName(m_loadedDicts[dict].codec);
+    QString translation;
+    if (codec)
+        translation = codec->toUnicode(http.readAll());
+    else
+        translation = QString::fromUtf8(http.readAll());
+
+    return Translation(dictionary, word, translation);
+}
+
+int Web::execSettingsDialog(QWidget *parent)
+{
+    ::SettingsDialog dialog(this, parent);
+    return dialog.exec();
+}
+
+Q_EXPORT_PLUGIN2(web, Web)
+
---
+++ plugins/web/web.h
@@ -0,0 +1,65 @@
+/******************************************************************************
+ * This file is part of the MULA project
+ * Copyright (C) 2008 Alexander Rodin
+ * Copyright (c) 2011 Laszlo Papp <lpapp@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef MULA_PLUGIN_WEB_WEB_H
+#define MULA_PLUGIN_WEB_WEB_H
+
+#include "dictionaryplugin.h"
+
+#include <QtCore/QHash>
+
+class Web: public QObject, public Mula::DictoinaryPlugin
+{
+    Q_OBJECT
+    Q_INTERFACES(Mula::DictionaryPlugin)
+
+    public:
+        Web(QObject *parent = 0);
+        virtual ~Web();
+
+        QString name() const;
+        QString version() const;
+        QString description() const;
+        QStringList authors() const;
+        Features features() const;
+
+        QStringList availableDicts() const;
+        QStringList loadedDicts() const;
+        void setLoadedDicts(const QStringList &dicts);
+        DictInfo dictInfo(const QString &dict);
+
+        bool isTranslatable(const QString &dict, const QString &word);
+        Translation translate(const QString &dict, const QString &word);
+
+        int execSettingsDialog(QWidget *parent);
+
+        friend class SettingsDialog;
+
+    private:
+        struct QueryStruct
+        {
+            QString query;
+            QByteArray codec;
+        };
+        QHash<QString, QueryStruct> m_loadedDicts;
+};
+
+#endif // MULA_PLUGIN_WEB_WEB_H
+
