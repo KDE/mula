069fae5845798946f143ac48b278afd141f04461
Laszlo Papp <ext-laszlo.papp@nokia.com>
2011-09-13 22:32:19 +0300

    Fix the include guard which blocked the pluginmanager include

--- core/dictionarydata.h
+++ core/dictionarydata.h
@@ -17,8 +17,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef MULA_CORE_PLUGINMANAGER_H
-#define MULA_CORE_PLUGINMANAGER_H
+#ifndef MULA_CORE_DICTIONARYDATA_H
+#define MULA_CORE_DICTIONARYDATA_H
 
 #include "dictionaryplugin.h"
 
